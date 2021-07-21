f3a75b8ea0fed384345e9225eeb1170ce1dae77e
P. Alexander Burnham <pburnham@uvm.edu>
2021-07-21 16:24:03 -0400

    new html

--- Theme3_PythonProjects/netCDF4_Ideas/netcdf4_scratch.py
+++ Theme3_PythonProjects/netCDF4_Ideas/netcdf4_scratch.py
@@ -1,7 +1,7 @@
 import netCDF4 as nc
 import numpy as np
 
-# locaate path to .nc4 test file
+# locate local path to .nc4 test file
 fn = "/Users/pburnham/Documents/GitHub/Theme3_LabNotebook/Theme3_PythonProjects/netCDF4_Ideas/data/prcp_1980.nc4"
 
 # create netcdf data set
@@ -12,4 +12,9 @@ prcp=ds['prcp'][:]
 
 # print a subset of the masked array
 print(prcp[0, 500:505, 200:205])
+
+# basic summary stats
+print(np.max(prcp))
+print(np.min(prcp))
+print(np.mean(prcp))
 
