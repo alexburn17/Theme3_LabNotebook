a4f9379a11a833a11f7d284eef234a355feb762a
P. Alexander Burnham <pburnham@uvm.edu>
2021-07-21 16:20:34 -0400

    playing with netcd4

--- Theme3_PythonProjects/netCDF4_Ideas/netcdf4_scratch.py
+++ Theme3_PythonProjects/netCDF4_Ideas/netcdf4_scratch.py
@@ -1,2 +1,15 @@
-import netCDF4
+import netCDF4 as nc
 import numpy as np
+
+# locaate path to .nc4 test file
+fn = "/Users/pburnham/Documents/GitHub/Theme3_LabNotebook/Theme3_PythonProjects/netCDF4_Ideas/data/prcp_1980.nc4"
+
+# create netcdf data set
+ds = nc.Dataset(fn)
+
+# Pull precipitation data out of the netcd4 object
+prcp=ds['prcp'][:]
+
+# print a subset of the masked array
+print(prcp[0, 500:505, 200:205])
+
