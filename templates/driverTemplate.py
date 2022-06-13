9dfbfb02cd5e2b8389fd25e0421c1ed47f574655
P. Alexander Burnham <pburnham@uvm.edu>
2022-06-13 14:17:55 -0400

    updaters

---
+++ templates/driverTemplate.py
@@ -0,0 +1,13 @@
+from spacetime.input.readData import read_data
+from spacetime.scale.rasterAlign import raster_align
+from spacetime.scale.rasterTrim import raster_trim
+from spacetime.objects.fileObject import file_object
+from spacetime.operations.cubeSmasher import cube_smasher
+from spacetime.operations.makeCube import make_cube
+from spacetime.operations.loadCube import load_cube
+from spacetime.graphics.dataPlot import plot_cube
+from spacetime.operations.time import cube_time, return_time, scale_time, select_time
+from spacetime.operations.cubeToDataframe import cube_to_dataframe
+import matplotlib.pyplot as plt
+
+# spacetime/python code here
